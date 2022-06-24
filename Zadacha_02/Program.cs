ea426559fac13967d6fb00a2b69d1a747c2a0b70
Яхина Наталья Андреевна <  @vvrz.local>
2022-06-24 15:45:28 +0300

    02

--- Zadacha_02/Program.cs
+++ Zadacha_02/Program.cs
@@ -3,3 +3,17 @@
 // a = 5; b = 7 -> max = 7
 // a = 2 b = 10 -> max = 10
 // a = -9 b = -3 -> max = -3
+
+Console.Write("введите первое число: ");
+int num1 = Convert.ToInt32 (Console.ReadLine());
+Console.Write("введите второе число: ");
+int num2 = Convert.ToInt32 (Console.ReadLine());
+
+if (num1>num2)
+{
+    Console.Write ($"максимальное: {num1}, минимальное: {num2}");
+}
+else
+{
+    Console.Write ($"максимальное: {num2}, минимальное: {num1}");
+}
