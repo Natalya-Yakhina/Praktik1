df62e05d99dfaf7934289dbe59b3d09b034fbeb1
Natalya <ivanechka93@mail.ru>
2022-06-25 13:43:14 +0300

    08

---
+++ Zadacha_08/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N),  
+//а на выходе показывает все четные числа от 1 до N
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+// Console.WriteLine("Введите число: ");
+// int N = Convert.ToInt32(Console.ReadLine()); // вводимое число N
+// int num = Convert.ToInt32(Console.ReadLine()); // переменная четных чисел на выходе
+// int a = 2; // условие от 1
+
+// for (a = num; a <= N; a++)
+//     {
+//     if (a % 2 == 0);
+//     }
+//     {
+// Console.WriteLine(a + " ");
+//     }
+
+Console.WriteLine("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine()); // вводимое число N
+int num = Convert.ToInt32(Console.ReadLine());
+int count = 1; // условие от 1
+
+while (count <= N)
+{
+    if (num % 2 == 0)
+    {
+        Console.Write($"{num}", num+2);
+    }
+    else
+    {
+        count = count+1;
+    }
+}
