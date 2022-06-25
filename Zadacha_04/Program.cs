383cf603a9fc7786306b806c1e2f2b7665d20a2a
Natalya <ivanechka93@mail.ru>
2022-06-25 11:50:12 +0300

    04

--- Zadacha_04/Program.cs
+++ Zadacha_04/Program.cs
@@ -7,34 +7,17 @@ Console.Write("введите первое число: ");
 int num1 = Convert.ToInt32(Console.ReadLine());
 Console.Write("введите второе число: ");
 int num2 = Convert.ToInt32(Console.ReadLine());
-// Console.Write("введите третье число: ");
-// int num3 = Convert.ToInt32(Console.ReadLine());
+Console.Write("введите третье число: ");
+int num3 = Convert.ToInt32(Console.ReadLine());
 
-int max = Convert.ToInt32(Console.ReadLine());
+int max = num1;
 
-if (num1 > num2)
+if (num2 > max)
 {
-    num1 = max;
+    Console.WriteLine($"Максимальное число: " {num2});
 }
-else
+else if (num3 > max)
 {
-    num2 = max;
+    Console.WriteLine($"Максимальное число: " {num3});
 }
-// }
-// if (num2 > num3)
-// {
-//     num2 = max;
-// }
-// else
-// {
-//     num3 = max;
-// }
-// if (num3 > num1)
-// {
-//     num3 = max;
-// }
-// else
-// {
-//     num1 = max;
-// }
-    Console.WriteLine($"максимальное число: {max}");
+Console.ReadKey();
