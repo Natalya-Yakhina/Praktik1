0df5486aaacbe172ce0e92977aad051b6efcbdbf
Natalya <ivanechka93@mail.ru>
2022-06-25 20:40:38 +0300

    08

--- Zadacha_08/Program.cs
+++ Zadacha_08/Program.cs
@@ -1,34 +1,14 @@
 ﻿// Задача 8: Напишите программу, которая на вход принимает число (N),  
-//а на выходе показывает все четные числа от 1 до N
+// а на выходе показывает все четные числа от 1 до N
 // 5 -> 2, 4
 // 8 -> 2, 4, 6, 8
 
-// Console.WriteLine("Введите число: ");
-// int N = Convert.ToInt32(Console.ReadLine()); // вводимое число N
-// int num = Convert.ToInt32(Console.ReadLine()); // переменная четных чисел на выходе
-// int a = 2; // условие от 1
-
-// for (a = num; a <= N; a++)
-//     {
-//     if (a % 2 == 0);
-//     }
-//     {
-// Console.WriteLine(a + " ");
-//     }
-
 Console.WriteLine("Введите число: ");
-int N = Convert.ToInt32(Console.ReadLine()); // вводимое число N
-int num = Convert.ToInt32(Console.ReadLine());
-int count = 1; // условие от 1
-
-while (count <= N)
+int N = Convert.ToInt32(Console.ReadLine());
+int i = 1;
+for (i = 1; i <= N; i++)
 {
-    if (num % 2 == 0)
-    {
-        Console.Write($"{num}", num+2);
-    }
-    else
-    {
-        count = count+1;
-    }
+    if (i % 2 == 0)
+    Console.Write($"{i}, ", i);
 }
+return 0;
