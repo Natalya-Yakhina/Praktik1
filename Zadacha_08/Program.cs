8472b8bbd9e3566d35d89f30a31e83b07a36c2e6
Яхина Наталья Андреевна <  @vvrz.local>
2022-06-27 08:35:05 +0300

    08_01

--- Zadacha_08/Program.cs
+++ Zadacha_08/Program.cs
@@ -4,11 +4,10 @@
 // 8 -> 2, 4, 6, 8
 
 Console.WriteLine("Введите число: ");
-int N = Convert.ToInt32(Console.ReadLine());
+int n = Convert.ToInt32(Console.ReadLine());
 int i = 1;
-for (i = 1; i <= N; i++)
+for (i = 1; i <= n; i++)
 {
     if (i % 2 == 0)
     Console.Write($"{i} ,", i);
 }
-return 0;
