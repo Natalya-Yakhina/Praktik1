882acc715cbc97a5a7bce01f533c889b9659a112
Наталья Яхина <ivanechka93@mail.ru>
2022-06-28 16:54:57 +0300

    Zadacha_13/

--- Zadacha_13/Program.cs
+++ Zadacha_13/Program.cs
@@ -4,46 +4,69 @@
 // 32679 -> 6
 
 
-Console.WriteLine("Введите число: ");
-int num = Convert.ToInt32(Console.ReadLine());
-int digitIndex = 2; // индекс третьей цифры
-
-if (num > 99)
-{
-    int digit = num.ToString()[digitIndex]- '0';
-    Console.WriteLine($"Третья цифра числа {num} = {digit}");
-}
-else
-{
-    Console.WriteLine("Третьей цифры нет");
-}
-
-
-
 // Console.WriteLine("Введите число: ");
 // int num = Convert.ToInt32(Console.ReadLine());
-// int numNew = 0;
+// int digitIndex = 2; // индекс третьей цифры
 
-// if (num > 100 && num < 999)  // от 100 до 999
-//     {
-//         numNew = (num % 10);
-//         Console.WriteLine($"Третья цифра числа {num} = {numNew}");
-//     }
-//     else
+// if (num > 99)
+// {
+//     int digit = num.ToString()[digitIndex]- '0';
+//     Console.WriteLine($"Третья цифра числа {num} = {digit}");
+// }
+// else
 // {
 //     Console.WriteLine("Третьей цифры нет");
 // }
-// if (num < 9999) 
-//     {
-//         numNew = (num / 10 % 10);
-//         Console.WriteLine ($"Третья цифра числа {num} = {numNew}"); 
-//     }
-// // if (num < 99999)
-// //     {
-// //         numNew = (num / 100 % 10);
-// //         Console.WriteLine ($"Третья цифра числа {num} = {numNew}");
-// //     }
-// else
+
+
+
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int numNew = 0; 
+int cnt = 0;
+for (int n = num; n > 0; n /= 10, cnt++);
+//  {
+//     Console.WriteLine($"Длинна числа: {cnt}"); // размер числа
+//  }
+
+if (cnt == 3)
+{
+    numNew = ((num % 10));
+    Console.WriteLine($"Третья цифра числа {num} = {numNew}");
+}
+else if (cnt <= 2)
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+if (cnt == 4)
+{
+     numNew = (num / 10 % 10);
+    Console.WriteLine ($"Третья цифра числа {num} = {numNew}");
+}
+if (cnt == 5)  
+{
+    numNew = (num / 100 % 10);
+    Console.WriteLine ($"Третья цифра числа {num} = {numNew}");
+}
+
+
+// второе решение не законченное
+// if (cnt == 3)
+// {
+//     numNew = (num % 10);
+//     Console.WriteLine($"Третья цифра числа {num} = {numNew}");
+// }
+// if (cnt == 4)
+// {
+//     numNew = (num / 10 % 10);
+//     Console.WriteLine ($"Третья цифра числа {num} = {numNew}");
+// }
+// if (cnt == 5)  
+// {
+//     numNew = (num / 100 % 10);
+//     Console.WriteLine ($"Третья цифра числа {num} = {numNew}");
+// }
+// else if (cnt<2)
 // {
 //     Console.WriteLine("Третьей цифры нет");
 // }
