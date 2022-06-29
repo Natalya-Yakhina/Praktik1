ad022ebca64e367127b670898947c148cb6616c4
Наталья Яхина <ivanechka93@mail.ru>
2022-06-29 15:07:59 +0300

    015

--- Zadacha_15/Program.cs
+++ Zadacha_15/Program.cs
@@ -7,13 +7,8 @@ Console.Write("Введите число: ");
 int num = Convert.ToInt32(Console.ReadLine());
 switch (num)
 {
-    case 1: Console.WriteLine("нет"); break;
-    case 2: Console.WriteLine("нет"); break;
-    case 3: Console.WriteLine("нет"); break;
-    case 4: Console.WriteLine("нет"); break;
-    case 5: Console.WriteLine("нет"); break;
-    case 6: Console.WriteLine("да"); break;
-    case 7: Console.WriteLine("да"); break;
+    case 1: case 2: case 3: case 4: case 5: Console.WriteLine("нет"); break;
+    case 6:case 7: Console.WriteLine("да"); break;
     default: Console.WriteLine("Нет такого дня недели"); break;
 }
 
