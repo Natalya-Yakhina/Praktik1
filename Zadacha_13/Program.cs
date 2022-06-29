7d064a49366f423a884574e981248d21e07d1acc
Наталья Яхина <ivanechka93@mail.ru>
2022-06-29 12:26:20 +0300

    013 финал

--- Zadacha_13/Program.cs
+++ Zadacha_13/Program.cs
@@ -4,33 +4,34 @@
 // 32679 -> 6
 
 
-Console.WriteLine("Введите число: ");
-int num = Convert.ToInt32(Console.ReadLine());
-int numNew = 0; 
-int cnt = 0;
-for (int n = num; n > 0; n /= 10, cnt++);
-//  {
-//     Console.WriteLine($"Длинна числа: {cnt}"); // размер числа
-//  }
-if (cnt == 3) 
-{
-    numNew = ((num % 10));
-    Console.WriteLine($"Третья цифра числа {num} = {numNew}");
-}
-else if (cnt <= 2)
-{
-    Console.WriteLine("Третьей цифры нет");
-}
-if (cnt == 4)
-{
-    numNew = (num / 10 % 10);
-    Console.WriteLine ($"Третья цифра числа {num} = {numNew}");
-}
-if (cnt == 5)
-{
-    numNew = (num / 100 % 10);
-    Console.WriteLine ($"Третья цифра числа {num} = {numNew}");
-}
+
+// Console.WriteLine("Введите число: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// int numNew = 0; 
+// int cnt = 0;
+// for (int n = num; n > 0; n /= 10, cnt++);
+// //  {
+// //     Console.WriteLine($"Длинна числа: {cnt}"); // размер числа
+// //  }
+// if (cnt == 3) 
+// {
+//     numNew = ((num % 10));
+//     Console.WriteLine($"Третья цифра числа {num} = {numNew}");
+// }
+// else if (cnt <= 2)
+// {
+//     Console.WriteLine("Третьей цифры нет");
+// }
+// if (cnt == 4)
+// {
+//     numNew = (num / 10 % 10);
+//     Console.WriteLine ($"Третья цифра числа {num} = {numNew}");
+// }
+// if (cnt == 5)
+// {
+//     numNew = (num / 100 % 10);
+//     Console.WriteLine ($"Третья цифра числа {num} = {numNew}");
+// }
 
 // ПОИСК ТРЕТЬЕЙ ЦИФРЫ ПО ИНДЕКСУ
 
@@ -47,3 +48,19 @@ if (cnt == 5)
 // {
 //     Console.WriteLine("Третьей цифры нет");
 // }
+
+
+Console.WriteLine("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int newNum3 = 0;
+if (n > 99)
+{
+    newNum3 = n % 10;
+    n = n / 10; 
+    Console.WriteLine($"Третья цифра числа = {newNum3}");
+}
+else
+{
+     Console.WriteLine("Третьей цифры нет"); 
+}
+
