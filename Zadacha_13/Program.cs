bfcff4792a0c815da95870ae3768bbfabb8c19d6
Наталья Яхина <ivanechka93@mail.ru>
2022-06-29 13:59:47 +0300

    задача 13

--- Zadacha_13/Program.cs
+++ Zadacha_13/Program.cs
@@ -3,35 +3,28 @@
 // 78 -> третьей цифры нет
 // 32679 -> 6
 
+Console.WriteLine("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int newNum3 = 0;
+if (n < 99)
+    {
+        Console.WriteLine("Третьей цифры нет");
+        return 0;
+    }
+while (n > 99) 
+{
+    newNum3 = n % 10;
+    n = n / 10;
+}
+    Console.WriteLine($"Третья цифра числа = {newNum3}");
+    return 0;
 
 
-// Console.WriteLine("Введите число: ");
-// int num = Convert.ToInt32(Console.ReadLine());
-// int numNew = 0; 
-// int cnt = 0;
-// for (int n = num; n > 0; n /= 10, cnt++);
-// //  {
-// //     Console.WriteLine($"Длинна числа: {cnt}"); // размер числа
-// //  }
-// if (cnt == 3) 
-// {
-//     numNew = ((num % 10));
-//     Console.WriteLine($"Третья цифра числа {num} = {numNew}");
-// }
-// else if (cnt <= 2)
-// {
-//     Console.WriteLine("Третьей цифры нет");
-// }
-// if (cnt == 4)
-// {
-//     numNew = (num / 10 % 10);
-//     Console.WriteLine ($"Третья цифра числа {num} = {numNew}");
-// }
-// if (cnt == 5)
-// {
-//     numNew = (num / 100 % 10);
-//     Console.WriteLine ($"Третья цифра числа {num} = {numNew}");
-// }
+
+
+
+
+
 
 // ПОИСК ТРЕТЬЕЙ ЦИФРЫ ПО ИНДЕКСУ
 
@@ -48,19 +41,3 @@
 // {
 //     Console.WriteLine("Третьей цифры нет");
 // }
-
-
-Console.WriteLine("Введите число: ");
-int n = Convert.ToInt32(Console.ReadLine());
-int newNum3 = 0;
-if (n > 99)
-{
-    newNum3 = n % 10;
-    n = n / 10; 
-    Console.WriteLine($"Третья цифра числа = {newNum3}");
-}
-else
-{
-     Console.WriteLine("Третьей цифры нет"); 
-}
-
