2d5200ccab2085d74897137d157bb62892bd1d24
Natalya <ivanechka93@mail.ru>
2022-07-02 19:05:03 +0300

    019

--- Zadacha_19/Program.cs
+++ Zadacha_19/Program.cs
@@ -5,25 +5,46 @@
 // 23432 -> да
 
 
-
-
 Console.Clear();
 Console.Write("Введите пятизначное число: ");
-string number = Console.ReadLine();
-while (number!.Length == 5)
+int num = Convert.ToInt32(Console.ReadLine());
+if (num < 10000 || num > 99999)
 {
-    if (number[0] == number[4] && number[1] == number[3])
-    {
-        Console.WriteLine($"Ваше число: {number} - палиндром.");
-    }
-    else
-    {
-        Console.WriteLine($"Ваше число: {number} - не палиндром.");
-    }
+    Console.WriteLine("ОШИБКА!");
     return;
 }
 
-// if (number!.Length > 5)
+int[] array = new int[5];
+int index = 0;
+while(index <= 4)
+{
+    array[index] = num % 10;
+    num = (num - array[index] / 10);
+    index++;
+}
+if(array[0] == array[4] && array[1] == array[3])
+{
+    Console.WriteLine("Ваше число - палиндром.");
+}
+else
+{
+    Console.WriteLine("Ваше число- не палиндром.");
+}
+
+
+
+// Console.Clear();
+// Console.Write("Введите пятизначное число: ");
+// string number = Console.ReadLine();
+// while (number!.Length == 5)
 // {
-//     Console.WriteLine("ОШИБКА!!!");
+//     if (number[0] == number[4] && number[1] == number[3])
+//     {
+//         Console.WriteLine($"Ваше число: {number} - палиндром.");
+//     }
+//     else
+//     {
+//         Console.WriteLine($"Ваше число: {number} - не палиндром.");
+//     }
+//     return;
 // }
