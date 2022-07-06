f1ddbf22d44bb8f316b316175fda0efd23960f99
Наталья Яхина <ivanechka93@mail.ru>
2022-07-06 11:59:00 +0300

    029

--- Zadacha_29/Program.cs
+++ Zadacha_29/Program.cs
@@ -3,38 +3,40 @@
 // 1, 2, 5, 7, 19, 6, 1, 33 -> [1, 2, 5, 7, 19, 6, 1, 33]
 
 
+// ====== вывод массива (с авто заполнением элементов) ======
+
 int[] array = new int[8]; // задаем массив из 8 эменентов
+
 void RandomArray(int[] array)
 {
     Random rand = new Random();
     Console.Write("[");
-    for(int i = 0; i < array.Length; i++)
+    for (int i = 0; i < array.Length; i++)
     {
         array[i] = rand.Next(0, 33); // ограничение для вывода не больших чисел = 33 (по макс из условия)
         Console.Write(array[i] + ",");
     }
-        Console.Write("]");
+    Console.Write("]");
 }
 int[] arrayNew = new int[8];
 RandomArray(array);
 
 
 
-// ====== вывод массива на экран с ручным вводом элементов =======
+// ====== вывод массива (с ручным вводом элементов) =======
 
+// int[] array = new int[8]; // задаем массив из 8 эменентов
 
-int[] array = new int[8]; // задаем массив из 8 эменентов
+// for (int i = 0; i < array.Length; i++)
+// {
+//     Console.Write($"Введите элемент массива под индексом {i}: ");
+//     array[i] = int.Parse(Console.ReadLine());
+// }
 
-for(int i = 0; i < array.Length; i++)
-{
-    Console.Write($"Введите элемент массива под индексом {i}: ");
-    array[i] = int.Parse(Console.ReadLine());
-}
+// Console.Write("Вывод массива: [");
 
-Console.Write("Вывод массива: [");
-
-for(int i = 0; i < array.Length; i++)
-{
-    Console.Write(array[i]+ ",");
-}
-Console.Write("]");
+// for (int i = 0; i < array.Length; i++)
+// {
+//     Console.Write(array[i] + ",");
+// }
+// Console.Write("]");
