6b52de62885af723996691df0a572b72adefb00a
Наталья Яхина <ivanechka93@mail.ru>
2022-07-06 16:52:51 +0300

    029

--- Zadacha_29/Program.cs
+++ Zadacha_29/Program.cs
@@ -5,22 +5,52 @@
 
 // ====== вывод массива (с авто заполнением элементов) ======
 
+// int[] array = new int[8]; // задаем массив из 8 эменентов
+
+// // void RandomArray(int[] array)
+// // {
+// //     Random rand = new Random();
+// //     Console.Write("[");
+// //     for (int i = 0; i < array.Length; i++)
+// //     {
+// //         array[i] = rand.Next(0, 33); // ограничение для вывода не больших чисел = 33 (по макс из условия)
+// //         Console.Write(array[i] + ",");
+// //     }
+// //     Console.Write("]");
+// // }
+// // int[] arrayNew = new int[8];
+// // RandomArray(array);
+
+// ====== разделение функционала ======
+
 int[] array = new int[8]; // задаем массив из 8 эменентов
 
-void RandomArray(int[] array)
+void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while(index < length)
+    {
+        collection[index] = new Random().Next(1, 10); // положить новое число из диапозона
+        index++;
+    }
+}
+
+void PrintArray(int[] col)
 {
     Random rand = new Random();
     Console.Write("[");
-    for (int i = 0; i < array.Length; i++)
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
     {
-        array[i] = rand.Next(0, 33); // ограничение для вывода не больших чисел = 33 (по макс из условия)
-        Console.Write(array[i] + ",");
+        Console.Write(col[position] + ",");
+        position++;
     }
     Console.Write("]");
 }
-int[] arrayNew = new int[8];
-RandomArray(array);
-
+FillArray(array);
+PrintArray(array);
 
 
 // ====== вывод массива (с ручным вводом элементов) =======
